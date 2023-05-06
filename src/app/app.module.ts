2061ea553c47e7146a57f1192d32d42fd45ba151
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-05-06 09:49:46 +0530

    Bootstrap Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Blog1Component } from './blog1/blog1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Blog1Component
   ],
   imports: [
     BrowserModule,
